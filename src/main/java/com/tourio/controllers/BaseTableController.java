f5505bcdbfbed348da5cca664b6bfb34bff20bb1
Bùi Trung Bảo <buitrbao222@gmail.com>
2021-11-23 01:59:51 +0700

    chore: add comments for base controllers

--- src/main/java/com/tourio/controllers/BaseTableController.java
+++ src/main/java/com/tourio/controllers/BaseTableController.java
@@ -12,10 +12,19 @@ public abstract class BaseTableController<T> implements Initializable {
     @FXML
     private TableView<T> table;
 
+    // Initialize how to render table columns and rows
     public abstract void initTable();
+
+    // Load data for table
     public abstract void loadData();
+
+    // On create button click
     public abstract void onCreateClick(ActionEvent e) throws Exception;
+
+    // On edit button click
     public abstract void onEditClick(ActionEvent e) throws Exception;
+
+    // On delete button click
     public abstract void onDeleteClick(ActionEvent e);
 
     @Override
